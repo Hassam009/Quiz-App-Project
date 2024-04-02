63aa7889612d082cfeb79f10e70162cd3ca2d582
Hassam Amer <hasideveloper@gmail.com>
2024-04-02 19:17:19 +0500

    Working on Logic

---
+++ QUIZ-APP-PROJECT.js
@@ -0,0 +1,17 @@
+const questions=[
+    {
+    'que':"Which of the Following is ML",
+    'a':"Html",
+    'b':"Css",
+    'a':"Js",
+    'correct':"a",
+},
+    {
+    'que':"Which of the Following for Styling",
+    'a':"Html",
+    'b':"Css",
+    'a':"Js",
+    'correct':"a",
+},
+
+]
