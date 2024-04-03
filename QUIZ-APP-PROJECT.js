e7659ebc3e34727de7d95826d4991e28e80d427b
Hassam Amer <hasideveloper@gmail.com>
2024-04-03 17:47:54 +0500

    Now Working but there are some issue which i will fix later

--- QUIZ-APP-PROJECT.js
+++ QUIZ-APP-PROJECT.js
@@ -3,16 +3,16 @@ const questions=[
     'que':"Which of the Following is ML",
     'a':"Html",
     'b':"Css",
-    'a':"Js",
+    'c':"Js",
     'correct':"a",
 },
     {
     'que':"Which of the Following for Styling",
     'a':"Html",
     'b':"Css",
-    'a':"Js",
-    'correct':"a",
-},
+    'c':"Js",
+    'correct':"b",
+}
 ]
 let index=0;
 let total=questions.length;
@@ -20,15 +20,17 @@ let right=0, wrong=0;
 const quesBox=document.getElementById("quesBox")
 const optionInput=document.querySelectorAll('.options')
 
-const loadQuestion=()=>{
-    if(index==total){
+const loadQuestion = () => {
+    if (index == total) {
         return endQuiz();
     }
     reset();
-    const data=questions[index]
-    quesBox.innerText=`${index+1})${data.que}`
-optionInput[0].nextElementSibling.innerHTML=data.a;
-optionInput[1].nextElementSibling.innerHTML=data.b;
+    const data = questions[index];
+    quesBox.innerText = `${index + 1}) ${data.que}`;
+    optionInput[0].nextElementSibling.innerHTML = data.a;
+    optionInput[1].nextElementSibling.innerHTML = data.b;
+    optionInput[2].nextElementSibling.innerHTML = data.c; // Add this line to populate option c
+    optionInput[3].nextElementSibling.innerHTML = data.d; // Add this line to populate option d
 }
 
 const submit=()=>{
@@ -61,12 +63,15 @@ optionInput.forEach(
 return answer
 }
 
-const endQuiz=()=>{
-    <h2>Thanks for Playing the quiz</h2>
+const endQuiz = () => {
+    quesBox.innerText = "Thanks for Playing the quiz";
 }
+
 
 const reset=()=>{
     optionInput.forEach((input)=>{
         input.checked=false;
     })
 }
+
+loadQuestion();
